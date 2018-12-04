ec4a164643c3b4a23e2b7761d277f81b773ab11a
Cortez <cortezguitar@gmail.com>
2018-12-04 21:10:58 +0200

    Navigation added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Wrapper>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Wrapper>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState(prevState => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+            });
+    }
+    
+    render () {
+        return (
+            <Wrapper>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} 
+                closed={this.SideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Wrapper>
+        )
+    }
+}
+
+export default Layout;
